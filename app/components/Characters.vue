a866e9b00657ff0ce60c1e471f658f229e85e3e4
Amélie Houel <amelie.houel@hetic.net>
2019-05-21 09:30:02 +0200

    Ajout data.json

--- app/components/Characters.vue
+++ app/components/Characters.vue
@@ -2,9 +2,11 @@
   <div class="big-header">
     <h1>{{ message }}</h1>
     <br />
-    <router-link class="button" to="/Characters"> Let's play</router-link>
+    <router-link class="button" to="/Game/1">Personnage 1</router-link>
+    <router-link class="button" to="/Game/2">Personnage 2</router-link>
   </div>
 </template>
+
 
 <script>
 import countService from '../services/countService';
@@ -12,10 +14,9 @@ import countService from '../services/countService';
 export default {
   data() {
     return {
-      message: 'Welcome to Evil'
+      message: 'Choisis ton Hero'
     };
   },
-
   methods: {
     handleClick() {
       countService.increment();
